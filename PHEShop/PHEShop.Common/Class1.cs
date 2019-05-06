75e79a917585e893ad812f605208baf4bc90d0f8
Pablo Garnica <pablogarnica@gmail.com>
2019-05-06 09:56:43 -0500

    20190506 - commit inicial

---
+++ PHEShop/PHEShop.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PHEShop.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ PHEShop/PHEShop.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PHEShop.UIClassic
+{
+    public class Class1
+    {
+    }
+}
